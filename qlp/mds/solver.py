5ebc1bca177d5c7311652a98edd49449a5b5ed95
Chris <chriskanii@gmail.com>
2020-01-22 09:45:03 -0800

    Added solver for qubo

---
+++ qlp/mds/solver.py
@@ -0,0 +1,54 @@
+"""QUBO solver routines to obtain global optimal solution.
+"""
+from typing import List, Tuple, Union, Optional
+from itertools import product
+
+import numpy as np
+
+from tqdm import tqdm
+
+
+def classical_search(
+    qubo: "SparseMatrix", n_nodes: Optional[int] = None
+) -> Union[int, List[Tuple[int]]]:
+    """
+
+    Assumes that the first n_nodes entries correspond to sorted vertex numbers.
+    """
+    energies = []
+    vecs = []
+
+    n_iterations = 2 ** qubo.shape[1]
+    if n_iterations > 10 ** 5:
+        go_on = input(
+            f"This search will execute {n_iterations} iterations. Continue (y/n)?"
+        )
+        if go_on.lower() != "y":
+            raise KeyboardInterrupt("Too many iterations abort.")
+
+    # Brute force iterate space
+    for vv in tqdm(product(*[(0, 1)] * qubo.shape[1])):
+        ee = (vv @ qubo @ vv).flatten()[0]
+        energies.append(ee)
+        vecs.append(vv)
+
+    # Find minimal energy
+    e_min = np.min(energies)
+    # And corresponding vectors
+    solutions = [tuple(vv) for vv, ee in zip(vecs, energies) if ee == e_min]
+
+    if n_nodes is not None:
+        # Compute energy for one of the solution vectors by only considering n_nodes
+        v0 = np.array(
+            [vv if vv in n < n_nodes else 0 for n, vv in enumerate(solutions[0])]
+        )
+        e_min = (v0 @ qubo @ v0).flatten()[0]
+        # Reduce solutions to n_nodes entries
+        solutions = [sol[:n_nodes] for sol in solutions]
+
+    # Convert bit list to vertex numbers
+    solutions = [
+        tuple([n for n, vv in enumerate(sol[:n_nodes]) if vv == 1]) for sol in solutions
+    ]
+
+    return e_min, solutions
