046127001bc8d7291413601798de6213bdd886c1
Chris <chriskanii@gmail.com>
2020-02-18 14:42:54 -0800

    Enforce python version 3

--- qlpdb/qlpdb/__init__.py
+++ qlpdb/qlpdb/__init__.py
@@ -1,12 +1,19 @@
 """Prepares the usage of the qlpdb module
 """
 import os
+import sys
 from django import setup as _setup
 
 
 def _init():
     """Initializes the django environment for qlpdb
     """
+    if sys.version_info.major < 3:
+        raise RuntimeError(
+            "You must run this code with Python major version 3"
+            " because we use hashing to store data."
+        )
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "qlpdb.config.settings")
     _setup()
 
