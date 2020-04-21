5f717de8abb71658f1cb675648fac9f30f68f741
ckoerber <software@ckoerber.com>
2020-04-21 20:40:21 +0200

    Integrals for Jason

--- qlpdb/qlpdb/tdse/views.py
+++ qlpdb/qlpdb/tdse/views.py
@@ -1,9 +1,12 @@
 from django.views.generic import DetailView, TemplateView
 from django.views.generic.list import ListView
 
+from numpy import roll
+
 from plotly.graph_objs import Scatter
 from plotly.subplots import make_subplots
 from plotly.offline import plot
+from plotly.express.colors import sequential
 
 from qlpdb.tdse.models import Tdse
 
@@ -42,13 +45,26 @@ class ComparisonView(TemplateView):
 
         fig = make_subplots(subplot_titles=["Probability", "Entropy"], cols=2)
 
-        for key, val in data.items():
+        for color, (key, val) in zip(["#1f77b4", "#ff7f0e"], data.items()):
+            dt = roll(val.time, -1)[:-1] - val.time[:-1]
+            s_int = (val.entropy[:-1] * dt).sum()
             fig.add_trace(
-                Scatter(x=val.time, y=val.prob, name=key, showlegend=False),
+                Scatter(
+                    x=val.time, y=val.prob, name=key, showlegend=False, line_color=color
+                ),
                 col=1,
                 row=1,
             )
-            fig.add_trace(Scatter(x=val.time, y=val.entropy, name=key), col=2, row=1)
+            fig.add_trace(
+                Scatter(
+                    x=val.time,
+                    y=val.entropy,
+                    name=f"{key:1.5f},\tInt(s(t)dt) = {s_int:1.3f}",
+                    line_color=color,
+                ),
+                col=2,
+                row=1,
+            )
 
         context["graph"] = plot(fig, auto_open=False, output_type="div")
 
