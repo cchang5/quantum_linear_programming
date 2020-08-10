b7c3b08d7a79a3d20f4f49ef14a7a9208ab06dca
Jason Chang <chiachang@berkeley.edu>
2020-08-10 16:01:30 -0700

    add spectrum to tdse offset check

--- paper/figures/ta_plots.py
+++ paper/figures/ta_plots.py
@@ -568,7 +568,7 @@ def getallspinconfig():
         for v in vrange:
             pr = np.zeros(2 ** n)
             for I in range(2 ** n):
-                dwstate = np.abs(np.array([int(i) for i in '{0:05b}'.format(I)]) - 1)  # map 0 <-> 1
+                dwstate = np.array([int(i) for i in '{0:05b}'.format(I)])
                 pr[I] = scount[at][v][(tuple(dwstate))]
             pr = pr / np.sum(pr)
             prdict[at][v] = pr
